5e26a5ae2aa8309bb8f3269a0c4f0286caaccd32
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-02-12 12:05:11 +0500

    sbdh

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
+import java.beans.Introspector;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            int n = Integer.parseInt(arg);
+            if (n < 0) return false;
+        }
+        return true;
     }
 }
